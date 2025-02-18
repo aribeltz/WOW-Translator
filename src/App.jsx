0b008158dc6d6e8819b0c652ffa71b983e1b5a3e
Ari <aribrashear@gmail.com>
2025-02-18 13:41:09 -0800

    Added a base URL and moved function to separate file to clean up App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,6 @@
 import { useState } from "react"
-import { languages } from "../languages"
-
-function getRandomArrIndex(arr) {
-  return arr[Math.floor(Math.random() * arr.length)]
-}
-
-function translateToWowLanguage(sentence, language) {
-  const languageData = languages[language]
-
-  function processSingleWord(word) {
-    const length = word.length
-    return languageData[length] ? getRandomArrIndex(languageData[length]) : word
-  }
-
-  function preserveCase(oldWord, newWord) {
-    if (oldWord === newWord) return oldWord
-
-    return newWord
-      .split("")
-      .map((char, index) =>
-        oldWord[index] === oldWord[index].toUpperCase()
-          ? char.toUpperCase()
-          : char.toLowerCase()
-      )
-      .join("")
-  }
-
-  function translateWord(word) {
-    const nonLetterRegex = /[^a-zA-Z\s]+/
-
-    // Exclude words with non-letters.
-    if (nonLetterRegex.test(word)) {
-      return word
-    }
-
-    // Check if the word exists in the dictionary.
-    const lowerCaseWord = word.toLowerCase()
-    if (languageData?.dictionary && languageData?.dictionary[lowerCaseWord]) {
-      return preserveCase(word, languageData.dictionary[lowerCaseWord])
-    }
-
-    const translatedWord = processSingleWord(word)
-    return preserveCase(word, translatedWord)
-  }
-
-  // Split the sentence into words, punctuation, and spaces.
-  const tokens = sentence.trim().match(/\w+|\s+|[^\w\s]/g) || []
-
-  // Translate one word at a time while preserving punctuation and spacing.
-  const translatedTokens = tokens.map((token) => {
-    if (/^\s+$/.test(token)) {
-      // Spacing
-      return token
-    } else if (/^[^\w\s]+$/.test(token)) {
-      // Punctuation
-      return token
-    } else {
-      return translateWord(token)
-    }
-  })
-
-  return translatedTokens.join("")
-}
+import { languages } from "src/languages"
+import { translateToWowLanguage } from "src/utils/translateToWowLanguage"
 
 export default function App() {
   const [language, setLanguage] = useState("default")
---
+++ src/utils/translateToWowLanguage.js
@@ -0,0 +1,63 @@
+import { languages } from "src/languages"
+
+export function translateToWowLanguage(sentence, language) {
+  function getRandomArrIndex(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+
+  const languageData = languages[language]
+
+  function processSingleWord(word) {
+    const length = word.length
+    return languageData[length] ? getRandomArrIndex(languageData[length]) : word
+  }
+
+  function preserveCase(oldWord, newWord) {
+    if (oldWord === newWord) return oldWord
+
+    return newWord
+      .split("")
+      .map((char, index) =>
+        oldWord[index] === oldWord[index].toUpperCase()
+          ? char.toUpperCase()
+          : char.toLowerCase()
+      )
+      .join("")
+  }
+
+  function translateWord(word) {
+    const nonLetterRegex = /[^a-zA-Z\s]+/
+
+    // Exclude words with non-letters.
+    if (nonLetterRegex.test(word)) {
+      return word
+    }
+
+    // Check if the word exists in the dictionary.
+    const lowerCaseWord = word.toLowerCase()
+    if (languageData?.dictionary && languageData?.dictionary[lowerCaseWord]) {
+      return preserveCase(word, languageData.dictionary[lowerCaseWord])
+    }
+
+    const translatedWord = processSingleWord(word)
+    return preserveCase(word, translatedWord)
+  }
+
+  // Split the sentence into words, punctuation, and spaces.
+  const tokens = sentence.trim().match(/\w+|\s+|[^\w\s]/g) || []
+
+  // Translate one word at a time while preserving punctuation and spacing.
+  const translatedTokens = tokens.map((token) => {
+    if (/^\s+$/.test(token)) {
+      // Spacing
+      return token
+    } else if (/^[^\w\s]+$/.test(token)) {
+      // Punctuation
+      return token
+    } else {
+      return translateWord(token)
+    }
+  })
+
+  return translatedTokens.join("")
+}
